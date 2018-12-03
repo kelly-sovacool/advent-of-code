8361a38c84220e0909937724e531d220ce25a59c
Kelly Sovacool <kellysovacool@gmail.com>
2018-12-02 20:25:57 -0500

    boilerplate for day 03

---
+++ day03/day03.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+
+
+def main():
+    with open('input.txt', 'r') as file:
+        input_list = [str(line) for line in file]
+    print('part 1:', input_list)
+    print('part 2:', input_list)
+
+
+def part1(input_list):
+    return
+
+
+def part1(input_list):
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ day03/test_day03.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+import unittest
+
+
+class TestDay03(unittest.TestCase):
+
+    def test_part1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
